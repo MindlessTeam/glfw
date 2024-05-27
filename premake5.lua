3dd3a381795ea178a6e3eadbd915dfe6f3b62609
The Beardless <ABeardless@gmx.net>
2024-05-28 01:13:49 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -10,21 +10,8 @@ project "GLFW"
 
     files
     {
-        "include/GLFW/glfw3.h",
-        "include/GLFW/glfw3native.h",
-        "src/glfw_config.h",
-        "src/context.c",
-        "src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-
-		"src/null_init.c",
-		"src/null_joystick.c",
-		"src/null_monitor.c",
-		"src/null_window.c",
-
-		"src/platform.c",
-		"src/window.c"
+        "include/**.h",
+		"src/**.c"
     }
 
     filter "system:windows"
